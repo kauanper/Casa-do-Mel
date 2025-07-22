34d414ecc70d8ca59dea16d7bd7e0cb5209ebe3b
kauan <kauancavalcante8p@gmail.com>
2025-07-22 03:18:33 -0300

    error: apagando instâncias

--- src/main/java/com/example/casadomel/controllers/ApicultorController.java
+++ src/main/java/com/example/casadomel/controllers/ApicultorController.java
@@ -3,6 +3,7 @@ package com.example.casadomel.controllers;
 import com.example.casadomel.entities.Apicultor;
 import com.example.casadomel.services.ListarApicultores;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.admin.SpringApplicationAdminJmxAutoConfiguration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,8 @@ public class ApicultorController {
 
     @GetMapping
     public ResponseEntity<List<Apicultor>> getApicultor() {
+
+        System.out.println("aaaaaa");
         List<Apicultor> response = listarApicultores.execute();
         return ResponseEntity.ok(response);
     }
--- src/main/java/com/example/casadomel/repositories/UsuariosRepository.java
+++ src/main/java/com/example/casadomel/repositories/UsuariosRepository.java
@@ -12,33 +12,6 @@ import java.util.List;
 public class UsuariosRepository {
     private List<Usuario> usuarios;
 
-    public UsuariosRepository() {
-        usuarios = new ArrayList<>();
-
-        Apicultor apicultor1 = new Apicultor();
-        apicultor1.setNome("Kauan");
-        apicultor1.setEmail("kauan@email.com");
-        apicultor1.setSenha("123");
-        apicultor1.setQuantidadeColmeias(5);
-        apicultor1.setPago(false);
-
-        Apicultor apicultor2 = new Apicultor();
-        apicultor2.setNome("Vitoria");
-        apicultor2.setEmail("vitoria@email.com");
-        apicultor2.setSenha("abc");
-        apicultor2.setQuantidadeColmeias(3);
-        apicultor2.setPago(false);
-
-        Administrador admin = new Administrador();
-        admin.setNome("Admin");
-        admin.setEmail("admin@email.com");
-        admin.setSenha("admin123");
-        admin.setSuperAdmin(false);
-
-        usuarios.add(apicultor1);
-        usuarios.add(apicultor2);
-        usuarios.add(admin);
-    }
 
     public List<Usuario> listarTodos() {
         return usuarios;
