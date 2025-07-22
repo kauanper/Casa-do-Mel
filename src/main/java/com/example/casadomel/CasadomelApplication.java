992e4e9d04952838f8741a52a9d3de2fadd1473c
kauan <kauancavalcante8p@gmail.com>
2025-07-22 02:24:00 -0300

    initializer

---
+++ src/main/java/com/example/casadomel/CasadomelApplication.java
@@ -0,0 +1,13 @@
+package com.example.casadomel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasadomelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasadomelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/casadomel/CasadomelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.casadomel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasadomelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
